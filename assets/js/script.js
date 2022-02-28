c87c5808222b4c446c93370f8b54892f0d4ed1c3
useriasminna <pal.iasmina@yahoo.com>
2022-02-28 18:13:38 +0000

    Delete game template literal and add event listener for Try another word button

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,93 +69,29 @@ document.addEventListener("DOMContentLoaded", function() {
  * Replace the initial menu from game section with the game content
  */
 function generateGameContent(activeLevelValue) {
-    document.getElementById("game-section").innerHTML = "";
 
-    // CREATES THE GAME HTML STRUCTURE
-    let gameBody = `
-            <div id="game-area">
-                <div id="upper-section">
-                    <!-- MELTING SNOWMAN CONTAINER -->
-                    <div id="cartoon-container">
-                        <img id="sun" src="assets/images/sun.png" alt="A smiling sun">
-                        <p><span id="snowman-life">100</span>% LIFE</p>
-                        <div id="melting-snowman-container">
-                            <img id="hat" src="assets/images/hat.png" alt="A black hat">
-                            <img class="body" src="assets/images/body.png" alt="A snowman body">
-                            <img id="nose" src="assets/images/nose.png" alt="A carrot as a nose">
-                            <img id="left-hand" src="assets/images/left-hand.png" alt="Snowman's left hand">
-                            <img id="right-hand" src="assets/images/right-hand.png" alt="Snowman's right hand">
-                        </div>
-                    </div>
-                    <!-- RANDOM WORD UNDERSCORES CONTAINER -->
-                    <div id="level-container">
-                        <p>Difficulty:</p>
-                        <p>${activeLevelValue}</p>
-                    </div>
-                    <div id="word-container">
-                        <p>${generateRandomWordUnderscores(activeLevelValue)}</p>
-                    </div>
-                    <div id="hint-container">
-                        <i class="fas fa-lightbulb"></i>
-                        <p id="hint"></p>
-                    </div>
-                </div>
-                <!-- ALPHABET CONTAINER -->
-                <div id="alphabet-container">
-                    <ul>
-                        <li>a</li>
-                        <li>b</li>
-                        <li>c</li>
-                        <li>d</li>
-                        <li>e</li>
-                        <li>f</li>
-                        <li>g</li>
-                        <li>h</li>
-                        <li>i</li>
-                        <li>j</li>
-                        <li>k</li>
-                        <li>l</li>
-                        <li>m</li>
-                        <li>n</li>
-                        <li>o</li>
-                        <li>p</li>
-                        <li>q</li>
-                        <li>r</li>
-                        <li>s</li>
-                        <li>t</li>
-                        <li>u</li>
-                        <li>v</li>
-                        <li>w</li>
-                        <li>x</li>
-                        <li>y</li>
-                        <li>z</li>
-                    </ul>
-                </div>
-            </div>
-            <!-- SCORE CONTAINER -->
-            <div id="score-container">
-                <p>Succes: <span>0</span></p>
-                <p>Failure: <span>0</span></p>
-            </div>
-            <div id="lowest-buttons-container">
-                <!-- TRY ANOTHER WORD BUTTON -->
-                <button class="btn" type="submit">TRY ANOTHER ONE!</button>
-                <!-- NEW GAME BUTTON -->
-                <button class="btn" type="submit">START NEW GAME!</button>
-            </div>
-   
-`
+    // DISPLAY GAME CONTENT
+    document.getElementById("game-prefferences").style.display = "none";
+    document.getElementById("game-elements").style.opacity = "1";
+    document.getElementById("game-elements").style.height = "auto";
+
+
 
     document.getElementsByTagName("header")[0].getElementsByTagName("h2")[0].innerText = "Guess the word and help the snowman rezist the heat"
     // CHANGE BODY STYLE 
     document.body.style.backgroundAttachment = "unset";
 
     // STYLE CONTENT-WRAP DIV TO FIX THE FOOTER AT THE BOTTOM OF THE PAGE
-    document.getElementById("content-wrap").style.paddingBottom = "90px";
+    document.getElementById("content-wrap").style.paddingBottom = "750px";
     document.getElementById("content-wrap").style.position = "relative"
     document.getElementById("content-wrap").style.zIndex = "-3"
 
-    document.getElementById("game-section").innerHTML = gameBody;
+    document.getElementById("level-container").getElementsByTagName("p")[1].innerText = activeLevelValue;
+    document.getElementById("word-container").getElementsByTagName("p")[0].innerText = generateRandomWordUnderscores(activeLevelValue);
+
+    document.getElementById("change-word-btn").addEventListener("click", function() {
+        document.getElementById("word-container").getElementsByTagName("p")[0].innerText = generateRandomWordUnderscores(activeLevelValue)
+    });
 }
 
 /**
@@ -230,6 +166,7 @@ function generateRandomWordUnderscores(level) {
             wordUnderscores += "_ "
     }
 
+    console.log(randomWord);
     return wordUnderscores;
 
 }
